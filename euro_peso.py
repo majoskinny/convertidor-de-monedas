8a4819d581cf8e5a33891e476ac6a06ad2895d3a
blackchero717@gmail.com <Jim232-cmd>
2025-12-18 09:53:19 -0600

    Agregue la parte de euros a pesos (Santiago Méndez García)

---
+++ euro_peso.py
@@ -0,0 +1,5 @@
+euros = float(input("Ingresa cuántos euros tienes: "))
+tipo_cambio = 21.10
+pesos = euros * tipo_cambio
+
+print(f"Si tienes {euros} euros, entonces tienes {pesos:.2f} pesos mexicanos")
